01a6a9873bf8caf01663251575c3947d249301b9
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-13 16:09:47 +0200

    02

---
+++ Ecommerce/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/dashboard")]
+    public class DashBoardController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
